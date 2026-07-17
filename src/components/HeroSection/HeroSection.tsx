5279af18837a91345a176d0282823d3cce30a6d3
Maria Bazhenova <yankeevsdixie@gmail.com>
2026-07-17 16:34:22 +0300

    feat: add HTML for component HeroSection

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -1,9 +1,34 @@
-import "./HeroSection.css"
+import "./HeroSection.css";
+import forestBird from "../../assets/images/hero-section-img.png"
 
 const HeroSection = () => {
-  return (
-    <div>HeroSection</div>
-  )
-}
+ return (
+  <section className="hero">
+   <div className="hero__container container">
+    <div className="hero__image-wrapper">
+     <img
+      className="hero__image"
+      src={forestBird}
+      alt="Лесная птица"
+     />
+    </div>
 
-export default HeroSection
+    <div className="hero__content">
+     <h1 className="hero__title">
+      Реплики картин <br />
+      <span className="hero__title-accent">от Ink. House</span>
+     </h1>
+     <p className="hero__description">
+      Высокое качество отрисовки на плотной бумаге или льняном холсте. Редкие
+      произведения, доступные цены.
+     </p>
+     <a href="#catalog" className="hero__button button button_theme_outlined">
+      Продукция
+     </a>
+    </div>
+   </div>
+  </section>
+ );
+};
+
+export default HeroSection;
