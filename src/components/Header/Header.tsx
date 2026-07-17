0de41a849f8311699ab00756d944115de591a1dc
Maria Bazhenova <yankeevsdixie@gmail.com>
2026-07-17 16:30:34 +0300

    feat: add HTML for component Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,50 @@
-import './Header.css'
+import "./Header.css";
+import star from "../../assets/Star 1.svg";
+import cartIcon from "../../assets/images/shopping-cart.png";
 
 const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+ return (
+  <header className="header">
+   <div className="header__container container">
+    <a href="/" className="header__logo logo">
+     <img
+      className="logo__icon"
+      src={star}
+      alt="Логотип Ink. House"
+     />
+     <span className="logo__text">Ink. House</span>
+    </a>
 
-export default Header
+    <nav className="header__nav nav">
+     <ul className="nav__list">
+      <li className="nav__item">
+       <a href="#reproductions" className="nav__link">
+        Репродукции
+       </a>
+      </li>
+      <li className="nav__item">
+       <a href="#new" className="nav__link">
+        Новинки
+       </a>
+      </li>
+      <li className="nav__item">
+       <a href="#about" className="nav__link">
+        О нас
+       </a>
+      </li>
+     </ul>
+    </nav>
+
+    <button className="header__cart cart-button" aria-label="Корзина покупок">
+     <img
+      className="cart-button__icon"
+      src={cartIcon}
+      alt="Иконка корзины"
+     />
+    </button>
+   </div>
+  </header>
+ );
+};
+
+export default Header;
